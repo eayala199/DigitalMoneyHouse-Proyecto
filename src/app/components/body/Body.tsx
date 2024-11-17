d338bbb8722dae3c8a83102ebfa201e3f7bbfa75
eayala199 <estanilaoa@gmail.com>
2024-11-17 18:17:24 -0300

    ultimas correcciones 2

--- src/app/components/body/Body.tsx
+++ src/app/components/body/Body.tsx
@@ -4,23 +4,12 @@ import Card from "../card/Card";
 const Body = () => {
   return (
     <div className="h-screen bg-cover bg-center relative" style={{ backgroundImage: "url('/assets/fondo-home.jpeg')" }}>
-      <style jsx>{`
-        @media (max-width: 375px) {
-          .text-responsive-h1 {
-            font-size: 28px;
-          }
-          .text-responsive-h2 {
-            font-size: 20px;
-          }
-        }
-      `}</style>
-
       <div className="flex flex-col justify-start h-full text-white px-8 pt-16">
         <div className="max-w-md">
-          <h1 className="text-[48px] text-left mb-4 text-responsive-h1">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-left mb-4">
             De ahora en adelante, hacés más con tu dinero
           </h1>
-          <h2 className="text-[34px] text-left text-lime-500 text-responsive-h2">
+          <h2 className="text-2xl sm:text-3xl md:text-4xl text-lime-500 text-left">
             Tu nueva <strong>billetera virtual</strong>
           </h2>
         </div>
